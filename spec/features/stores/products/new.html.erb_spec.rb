42ba04a5c8ead8b46d43a4b0a6e56a24c42f983a
Sandisz Thieme <sandiszthieme@gmail.com>
2022-03-25 23:37:39 -0600

    Test: Store product new page has a form to create a new product

--- spec/features/stores/products/new.html.erb_spec.rb
+++ spec/features/stores/products/new.html.erb_spec.rb
@@ -14,5 +14,35 @@ require 'rails_helper'
 # and I am redirected to the Parent Childs Index page where I can see the new child listed
 
 RSpec.describe "Store Product creation" do
+  before :each do
+    @store_1 = Store.create!(
+      name: "Store 1",
+      description: "The store one",
+      address: "123 fake st.",
+      rating: 1,
+      sale: false
+    )
+    @store_2 = Store.create!(
+      name: "Store 2",
+      description: "The store two",
+      address: "123 real st.",
+      rating: 5,
+      sale: false
+    )
+  end
+  it 'has a form to create a new product for that store' do
+    visit "/stores/#{@store_1.id}/products"
+
+    click_button("Add product to #{@store_1.name}")
+    expect(current_path).to eq("/stores/#{@store_1.id}/products/new")
+
+    fill_in('Name', with: 'Test Product')
+    fill_in('Description', with: 'A test product for testing')
+    fill_in('Price', with: 5.03)
+    fill_in('Quantity', with: 8)
+
+    expect(current_path).to eq("/stores/#{@store_1.id}/products")
+    expect(page).to have_content("Test Product")
+  end
 
 end
