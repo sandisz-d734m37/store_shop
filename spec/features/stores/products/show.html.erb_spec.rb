8caf19819e2c7218de0f986133008c78d5eebb66
Sandisz Thieme <sandiszthieme@gmail.com>
2022-03-26 12:42:33 -0600

    Test: Store Product Show page has all product information

---
+++ spec/features/stores/products/show.html.erb_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "Store Product show page" do
+  before(:each) do
+      @store_1 = Store.create!(
+        name: "Store 1",
+        description: "The store one",
+        address: "123 fake st.",
+        rating: 1,
+        sale: false
+      )
+      @store_2 = Store.create!(
+        name: "Store 2",
+        description: "The store two",
+        address: "123 real st.",
+        rating: 5,
+        sale: false
+      )
+
+      @product_1 = @store_1.products.create!(
+        name: "product 1",
+        description: "first product",
+        price: 1.04,
+        quantity: 1,
+        on_sale: true
+      )
+      @product_2 = @store_2.products.create!(
+        name: "product 2",
+        description: "second product",
+        price: 35.04,
+        quantity: 80010,
+        on_sale: false
+      )
+  end
+  it 'has all products information' do
+    visit "/stores/#{@store_1.id}/products/#{@product_1.id}"
+
+    expect(page).to have_content(@product_1.name)
+    expect(page).to have_content(@product_1.description)
+    expect(page).to have_content(@product_1.price)
+    expect(page).not_to have_content(@product_2.name)
+  end
+end
